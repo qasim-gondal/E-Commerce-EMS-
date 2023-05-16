e91894eee48f182d12f4db500fa014496b10511c
Your Name <you@example.com>
2023-05-16 20:19:32 +0500

    admin wip

---
+++ test/controllers/admins_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class AdminsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get admins_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get admins_create_url
+    assert_response :success
+  end
+end
