d447a211a9e91c3b5b5dc8d93461ffee8d94633b
Your Name <you@example.com>
2023-05-17 16:04:16 +0500

    Revert "modification in admins"
    This reverts commit a0bd84979a698d21822934718025b989b1aff499.
    
    	modified:   app/controllers/admins_controller.rb
    	deleted:    app/views/admins/index.html.erb
    	modified:   config/routes.rb

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,7 +1,4 @@
 class AdminsController < ApplicationController
-  def index
-  end
-
   def new
     @admin = Admin.new
   end
@@ -10,7 +7,7 @@ class AdminsController < ApplicationController
     @admin = Admin.new(admin_params)
     if @admin.save
       # Handle successful admin creation
-      redirect_to admins_path, notice: "Admin created successfully."
+      redirect_to root_path, notice: "Admin created successfully."
     else
       render :new
     end
